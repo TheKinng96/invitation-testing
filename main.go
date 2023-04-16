5581db267248432ea52b7d50249ba2667e332902
G3N <fengyuan.yap@gmail.com>
2023-04-17 08:22:07 +0900

    added email function

--- main.go
+++ main.go
@@ -2,12 +2,32 @@ package main
 
 import (
 	"log"
+	"net/mail"
 
 	"github.com/pocketbase/pocketbase"
+	"github.com/pocketbase/pocketbase/core"
+	"github.com/pocketbase/pocketbase/tools/hook"
+	"github.com/pocketbase/pocketbase/tools/mailer"
 )
 
 func main() {
 	app := pocketbase.New()
+
+	app.OnMailerBeforeRecordVerificationSend().Add(func(e *core.MailerRecordEvent) error {
+		// send custom email
+		err := e.MailClient.Send(&mailer.Message{
+				From:    mail.Address{Address: "support@example.com"},
+				To:      mail.Address{Address: e.Record.Email()},
+				Subject: "YOUR_SUBJECT...",
+				HTML:    "YOUR_HTML_BODY...",
+		})
+
+		if err != nil {
+				return err
+		}
+
+		return hook.StopPropagation
+})
 
 	if err := app.Start(); err != nil {
 			log.Fatal(err)
